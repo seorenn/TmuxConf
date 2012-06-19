05868a422012be564edc1b94c0f7c3378260d714
Heeseung Seo <hirenn@websync.co.kr>
2012-06-19 17:19:45 +0900

    removed default creation of panes

--- mytmux.sh
+++ mytmux.sh
@@ -12,11 +12,11 @@ tmux new-session -d -s $SESSION
 
 # Create default panes
 
-tmux split-window -h
-tmux split-window -v
+#tmux split-window -h
+#tmux split-window -v
 
 # TODO
 
-tmux select-window -t $SESSION:0
+#tmux select-window -t $SESSION:0
 tmux attach -t $SESSION
-tmux select-pane -t 0
+#tmux select-pane -t 0
